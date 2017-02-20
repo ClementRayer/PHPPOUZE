80f0fccf97afe80cb274c2967332a524c7f00756
Unknown <clement.rayer@gmail.com>
2017-02-20 19:24:50 +0100

    Setup full

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
+<!doctype html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="style/style.css">
+    <title>Dungeon Raider</title>
 </head>
-<ul>
-    <li class="koi">
-<?php
 
-    echo 'KOI';
-?>
-    </li>
-    <li class="koman">
-        <?php
-
-        echo '<hr>KOMAN';
-        ?>
-    </li>
-</ul>
+<body>
+    <p>Are you ready to start playing?</p>
+    <button>START!</button>
+</body>
+</html>
