b42cf805c91ee805b479548d0e1cadec0b98fb72
Unknown <clement.rayer@gmail.com>
2017-02-22 14:39:06 +0100

    Setup characters

--- class/characters.php
+++ class/characters.php
@@ -1,37 +1,44 @@
 <?php
 
-class Characters {
+class Characters extends Game{
+    public $name;
     public $atk;
     public $magic;
     public $def;
     public $life;
     public $stuff;
+
+    public function __construct(array $stats) {
+        new $this->$name;
+        new $this->$atk;
+        new $this->$magic;
+        new $this->$def;
+        new $this->$life;
+        new $this->$stuff;
+    }
 }
 
 class Warrior extends Characters {
-    public $warriorAtk->$atk;
-
-    public function attack($target){
-        $target->takeDamage($this->$atk);
+    public function __set($stats, $value)
+    {
+        parent::__construct($inputName, '90', '0', '80', '100', $item);
     }
 }
 
 class Wizard extends Characters {
-    public $paladinMagic->$magic;
-
-    public function castSpell($target){
-        $target->takeDamage($this->$magic);
+    public function __set($stats, $value)
+    {
+        parent::__construct($inputName, '0', '90', '30', '80', $item);
+        $this->Characters = array($inputName, '0', '90', '30', '80', $item);
     }
+
 }
 
 class Paladin extends Characters {
-    public $paladinAtk->$atk;
-    public $paladinMagic->$magic;
-    
-    public function attack($target){
-        $target->takeDamage($this->$atk);
+    public function __set($stats, $value)
+    {
+        parent::__construct($inputName, '70', '50', '60', '100', $item);
     }
-    public function castSpell($target){
-        $target->takeDamage($this->$magic);
+
     }
 }
