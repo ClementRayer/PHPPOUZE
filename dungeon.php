ceb7cdf42a8521e859332715745534329069486a
Unknown <thomaspericoi@gmail.com>
2017-02-26 23:21:06 +0100

    Oui

--- dungeon.php
+++ dungeon.php
@@ -2,8 +2,8 @@
 
 session_start();
 
-//error_reporting(E_ALL);
-//ini_set("display_errors", 1);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 require_once 'class/Game.php';
 require_once 'class/Salle.php';
